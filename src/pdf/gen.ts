5b806ad68b67f812a52095524ccebd3cbcf71b9b
AlexStorm <info@alexstorm.ru>
2023-12-19 16:16:21 +0530

    gen

--- src/pdf/gen.ts
+++ src/pdf/gen.ts
@@ -1,19 +1,50 @@
-const puppeteer = require('puppeteer');
+import puppeteer from "puppeteer";
+import {marked} from "marked";
+import * as buffer from "buffer";
+
 
 async function gen(filePath: string) {
     const browser = await puppeteer.launch();
 
     const page = await browser.newPage();
 
-    await page.setContent('<h1>Hello, Puppeteer!</h1>');
+    const html = await Bun.file("./index.html").text()
+
+    // Read Markdown content from a file
+    const markdownContent = await Bun.file("./README.md").text();
+
+
+
+
+// Convert Markdown to HTML
+    const htmlContent = await marked(markdownContent);
+
+
+   const injectedHtml = html.replace("{{content}}", htmlContent)
+
+  // console.log("INJECTED",injectedHtml)
+
+    await page.setContent(injectedHtml);
+
+
     const readableStream = await page.createPDFStream({format: 'A4'});
-    readableStream.on('data', async (chunk: any) => {
-        Bun.file(filePath, chunk)
-        await Bun.write(filePath, chunk);
-    });
+
+    const chunks: Buffer[] = [];
+
+  await   readableStream.forEach((chunk: Buffer) => {
+        console.log('Got a chunk of data:', chunk.length);
+      chunks.push(chunk);
+
+    })
     await browser.close();
+
+   const joinedBuffer= Buffer.concat(chunks);
+
+
+    await Bun.write(filePath,joinedBuffer );
 
     console.log('Here\'s your PDF!');
 }
 
-gen("whitepaper.pdf");
+
+gen("Solenopsys.pdf");
